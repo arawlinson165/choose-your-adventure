d10d36501f6f86ba6d4529256908446f07ed7864
arawlinson165 <arawlinson165@gmail.com>
2026-06-23 23:36:04 -0500

    comment out rep score and add files

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 //script for scenario 1
-let points;
+let scoreDisp = document.querySelector("#points");
+let points = 0;
+let pos = document.querySelector("#pos");
+let neg = document.querySelector("#neg");
+
+let pos1 = document.querySelector("#pos1");
+let neg1 = document.querySelector("#neg1");
 
 //give points-giving buttons an id so js can find them
 
 
 //run off in different directions
-b1.addEventListener("click", function () {
-    document.querySelector(".page page-0").style.display = "none";
+
+pos.addEventListener("click", function(event){
+    points += 1;
+    scoreDisp.innerHTML = points + " reputation score";
+    console.log(points);
 })
 
-//everyone look for water
-b2.addEventListener("click", function () {
-    bg.className = "page page-2_2";
-    s.innerHTML = "You've been separated from the group for a few hours, but you find a cool bird that starts to follow you."
-    b1.innerHTML = "1. Start chirping at it to try to learn its language";
-    b2.innerHTML = "2. Try to k*ll it and eat it because you're getting hangry";
-    b3.innerHTML = "";
-    b4.innerHTML = "4. Ignore it";
-
-
-
+neg.addEventListener("click", function(event){
+    points -= 1;
+    scoreDisp.innerHTML = points + " reputation score";
 })
 
+pos1.addEventListener("click", function(event){
+    points += 1;
+    scoreDisp.innerHTML = points + " reputation score";
+    console.log(points);
+})
+
+neg1.addEventListener("click", function(event){
+    points -= 1;
+    scoreDisp.innerHTML = points + " reputation score";
+})
